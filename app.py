176072befb6d97de5d987357494e06e2fa9f2508
PavaraM <pavaramirihagalla@gmail.com>
2026-05-13 23:31:02 +0530

    feat: update message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"status": "ok", "message": "hello from cicd-demo"})
+    return jsonify({"status": "ok", "message": "cicd pipeline is working"})
 
 @app.route("/health")
 def health():
